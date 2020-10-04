5ff3c3d8d167362701d21056e37558c53b34ccf2
Momtchil Momtchev <momtchil@momtchev.com>
2020-10-04 22:54:04 +0200

    add unit testing for the Promise version

--- test/api_rasterband-async.test.js
+++ test/api_rasterband-async.test.js
@@ -11,13 +11,27 @@ describe('gdal.RasterBand', () => {
   })
   describe('instance', () => {
     describe('"pixels" property', () => {
-      describe('read()', () => {
+      describe('readAsync()', () => {
         it('should return a TypedArray', () => {
           const ds = gdal.openAsync(`${__dirname}/data/sample.tif`, undefined, (e, ds) => {
             const band = ds.bands.get(1)
             const w = 20
             const h = 30
             band.pixels.readAsync(190, 290, w, h, undefined, undefined, undefined, undefined, undefined, undefined, (e, data) => {
+              assert.instanceOf(data, Uint8Array)
+              assert.equal(data.length, w * h)
+              assert.equal(data[10 * 20 + 10], 10)
+            });
+          })
+        })
+      })
+      describe('readPromise()', () => {
+        it('should return a TypedArray', () => {
+          const ds = gdal.openPromise(`${__dirname}/data/sample.tif`).then(ds => {
+            const band = ds.bands.get(1)
+            const w = 20
+            const h = 30
+            band.pixels.readPromise(190, 290, w, h).then(data => {
               assert.instanceOf(data, Uint8Array)
               assert.equal(data.length, w * h)
               assert.equal(data[10 * 20 + 10], 10)
