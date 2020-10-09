279c90f59ae084289a600f2638485b2c24c0f3b0
Momtchil Momtchev <momtchil@momtchev.com>
2020-10-09 11:51:36 +0200

    Use char on GDAL<2.3 and const char on GDAL>=2.3

--- src/gdal_spatial_reference.hpp
+++ src/gdal_spatial_reference.hpp
@@ -20,6 +20,12 @@ using namespace node;
 
 namespace node_gdal {
 
+#if GDAL_VERSION_MAJOR < 2 || (GDAL_VERSION_MAJOR <= 2 && GDAL_VERSION_MINOR < 3)
+typedef char OGRChar;
+#else
+typedef const char OGRChar;
+#endif
+
 class SpatialReference : public Nan::ObjectWrap {
     public:
   static Nan::Persistent<FunctionTemplate> constructor;
