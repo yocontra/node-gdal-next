28d1116c11d6039e94feb6ae31f929174cac868b
Momtchil Momtchev <momtchil@momtchev.com>
2020-11-23 21:57:47 +0100

    feat: add initial support for using JS buffers as GDAL files

---
+++ src/gdal_memfile.cpp
@@ -0,0 +1,42 @@
+#include "gdal_memfile.hpp"
+
+namespace node_gdal {
+
+std::map<void *, Memfile *> Memfile::memfile_collection;
+
+Memfile::Memfile(void *data, size_t len) : data(data), len(len) {
+  char _filename[32];
+  // The pointer makes for a perfect unique filename
+  snprintf(_filename, sizeof(_filename), "/vsimem/%p", data);
+  this->filename = _filename;
+}
+
+void Memfile::weakCallback(const Nan::WeakCallbackInfo<Memfile> &file) {
+  Memfile *mem = file.GetParameter();
+  memfile_collection.erase(mem->data);
+  VSIUnlink(mem->filename.c_str());
+  delete mem;
+}
+
+Memfile *Memfile::get(Local<Object> buffer) {
+  void *data = node::Buffer::Data(buffer);
+  if (memfile_collection.count(data)) return memfile_collection.find(data)->second;
+
+  size_t len = node::Buffer::Length(buffer);
+  Memfile *mem = new Memfile(data, len);
+
+  VSILFILE *vsi = VSIFileFromMemBuffer(mem->filename.c_str(), (GByte *)data, len, 0);
+  if (mem->data == nullptr) {
+    delete mem;
+    return nullptr;
+  }
+  VSIFCloseL(vsi);
+
+  mem->weakHandle = new Nan::Persistent<Object>(buffer);
+  mem->weakHandle->SetWeak(mem, weakCallback, Nan::WeakCallbackType::kParameter);
+
+  memfile_collection[data] = mem;
+  return mem;
+}
+
+} // namespace node_gdal
---
+++ src/gdal_memfile.hpp
@@ -0,0 +1,38 @@
+#ifndef __NODE_GDAL_MEMFILE_H__
+#define __NODE_GDAL_MEMFILE_H__
+
+// node
+#include <node.h>
+#include <node_buffer.h>
+#include <node_object_wrap.h>
+
+// nan
+#include "nan-wrapper.h"
+
+// gdal
+#include <gdal_priv.h>
+
+#include "utils/obj_cache.hpp"
+
+using namespace v8;
+using namespace node;
+
+// A vsimem file
+
+namespace node_gdal {
+
+class Memfile {
+  void *data;
+  size_t len;
+  Nan::Persistent<Object> *weakHandle;
+  static void weakCallback(const Nan::WeakCallbackInfo<Memfile> &);
+
+    public:
+  std::string filename;
+  Memfile(void *, size_t);
+  static Memfile *get(Local<Object>);
+  void release();
+  static std::map<void *, Memfile *> memfile_collection;
+};
+} // namespace node_gdal
+#endif
