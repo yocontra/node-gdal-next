7d753c85f259e96640d593df785d0e4c338993e2
Momtchil Momtchev <momtchil@momtchev.com>
2021-05-26 16:00:33 +0200

    Add WMS/WMTS unit tests

---
+++ test/open_wms.test.ts
@@ -0,0 +1,55 @@
+import * as gdal from '..'
+import * as chai from 'chai'
+const assert = chai.assert
+import * as chaiAsPromised from 'chai-as-promised'
+chai.use(chaiAsPromised)
+
+// https://magosm.magellium.com/aide.html © Magellium pour les flux WMS/WFS
+
+const magosm = 'https://magosm.magellium.com/geoserver/ows?SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&LAYERS=magosm%3Afrance_telecom_fibre_connection_point&CRS=CRS:84&BBOX=2,48,3,49'
+
+describe('Open', () => {
+  afterEach(global.gc)
+
+  describe('WMS', () => {
+    let ds: gdal.Dataset
+
+    after(() => (ds && ds.close()))
+
+    it('should not throw', () => {
+      ds = gdal.open(magosm)
+    })
+
+    it('should be able to the bands', () => {
+      assert.equal(ds.bands.count(), 3)
+      assert.equal(ds.bands.get(1).overviews.get(19).size.x, 1024)
+    })
+
+    it('should have projection', () => {
+      assert.isTrue(ds.srs.isSame(gdal.SpatialReference.fromEPSG(4326)))
+    })
+  })
+
+  describe('WMS/Async', () => {
+    let ds: Promise<gdal.Dataset>
+
+    after(() => ds && ds.then((r) => r.close()))
+
+    it('should not throw', () => {
+      ds = gdal.openAsync(magosm)
+      return assert.isFulfilled(ds)
+    })
+
+    it('should be able to read raster size', () =>
+      assert.isFulfilled(Promise.all([
+        assert.eventually.equal(ds.then((r) => r.bands.count()), 3),
+        assert.eventually.equal(ds.then((r) => r.bands.get(1).overviews.get(19).size.x), 1024)
+      ]))
+    )
+
+    it('should have projection', () =>
+      assert.eventually.isTrue(ds.then((r) =>
+        r.srs.isSame(gdal.SpatialReference.fromEPSG(4326))
+      )))
+  })
+})
---
+++ test/open_wmts.test.ts
@@ -0,0 +1,53 @@
+import * as gdal from '..'
+import * as chai from 'chai'
+const assert = chai.assert
+import * as chaiAsPromised from 'chai-as-promised'
+chai.use(chaiAsPromised)
+
+const shomTides = 'WMTS:https://services.data.shom.fr/INSPIRE/wmts?request=GetCapabilities&service=WMTS&version=1.0.0,layer=ZONES_MAREE_PYR_PNG_3857_WMTS'
+
+describe('Open', () => {
+  afterEach(global.gc)
+
+  describe('WMTS', () => {
+    let ds: gdal.Dataset
+
+    after(() => (ds && ds.close()))
+
+    it('should not throw', () => {
+      ds = gdal.open(shomTides)
+    })
+
+    it('should be able to the bands', () => {
+      assert.equal(ds.bands.count(), 4)
+      assert.equal(ds.bands.get(1).overviews.get(16).size.x, 234)
+    })
+
+    it('should have projection', () => {
+      assert.isTrue(ds.srs.isSame(gdal.SpatialReference.fromEPSG(3857)))
+    })
+  })
+
+  describe('WMTS/Async', () => {
+    let ds: Promise<gdal.Dataset>
+
+    after(() => ds && ds.then((r) => r.close()))
+
+    it('should not throw', () => {
+      ds = gdal.openAsync(shomTides)
+      return assert.isFulfilled(ds)
+    })
+
+    it('should be able to read raster size', () =>
+      assert.isFulfilled(Promise.all([
+        assert.eventually.equal(ds.then((r) => r.bands.count()), 4),
+        assert.eventually.equal(ds.then((r) => r.bands.get(1).overviews.get(16).size.x), 234)
+      ]))
+    )
+
+    it('should have projection', () =>
+      assert.eventually.isTrue(ds.then((r) =>
+        r.srs.isSame(gdal.SpatialReference.fromEPSG(3857))
+      )))
+  })
+})
