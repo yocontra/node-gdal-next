c0c2acd8b8a4a31b25648b9e2a7c1255285858a8
Momtchil Momtchev <momtchil@momtchev.com>
2021-10-21 15:28:25 +0200

    find a way around the builtin bison rules in make

--- deps/libhdf5.sh
+++ deps/libhdf5.sh
@@ -20,7 +20,10 @@ tar -xzf hdf5-${HDF5_VERSION}.tar.gz
 mv hdf5-hdf5-`echo ${HDF5_VERSION} | tr "." "_"` $dir_hdf5
 cp H5pubconf.h $dir_hdf5/src/H5pubconf.h
 
-#disable an obnoxious warning
-patch $dirhdf5/src/H5trace.c < patches/H5trace.diff
+# make, called by gyp, called by node-gyp has a built-in rule that will sometimes mess with .l/.y files
+find ${dir_hdf5} -name *.[ly] -delete
+
+# disable an obnoxious warning
+patch ${dir_hdf5}/src/H5trace.c < patches/H5trace.diff
 
 [ -d ${dir_hdf5} ] && rm -rf ${dir_hdf5}/{tools,test,testpar}
