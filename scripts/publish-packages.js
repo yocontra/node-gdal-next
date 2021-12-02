186265cdbdc00e4fc1427663d5db6372efcf1faf
Momtchil Momtchev <momtchil@momtchev.com>
2021-12-02 13:50:15 +0100

    check only the latest publish run & sanitize input

--- scripts/publish-packages.js
+++ scripts/publish-packages.js
@@ -3,7 +3,8 @@ const { Octokit } = require('@octokit/core')
 const octokit = new Octokit({ auth: process.env.NODE_PRE_GYP_GITHUB_TOKEN })
 const pkg = { repo: 'node-gdal-async', owner: 'mmomtchev' }
 const version = require('../package.json').version
-const workflowPublish = { ...pkg, workflow_id: 7048427, ref: `v${version}` };
+const workflowPublishId = 7048427
+const workflowPublish = { ...pkg, workflow_id: workflowPublishId, ref: `v${version}` };
 
 (async () => {
   const branch = (await exec('git branch --show-current')).stdout.trim()
@@ -16,12 +17,15 @@ const workflowPublish = { ...pkg, workflow_id: 7048427, ref: `v${version}` };
     // eslint-disable-next-line no-await-in-loop
     await new Promise((res) => setTimeout(res, 5000))
     // eslint-disable-next-line no-await-in-loop
-    status = await octokit.request('GET /repos/{owner}/{repo}/actions/runs', { ...workflowPublish, per_page: 1, page: 0 })
+    status = await octokit.request(`GET /repos/{owner}/{repo}/actions/workflows/${workflowPublishId}/runs`,
+      { ...pkg, per_page: 1, page: 0 })
   } while (status.data.workflow_runs[0].status !== 'completed')
   process.stdout.write('\n')
   if (status.data.workflow_runs[0].conclusion !== 'success') {
     const url = new URL(status.data.workflow_runs[0].jobs_url)
-    if (url.protocol !== 'https:') throw new Error(`unexpected protocol in ${url}`)
+    if (url.protocol !== 'https:' || url.hostname !== 'api.github.com' || url.pathname.match(/^[a-z0-9\/\-]+$/g)) {
+      throw new Error(`unexpected protocol URL ${url}`)
+    }
     const jobs = await octokit.request(url.toString())
     const failedJob = jobs.data.jobs.filter((j) => ![ 'success', 'cancelled' ].includes(j.conclusion))
     const failedStep = failedJob[0].steps.filter((j) => ![ 'success', 'cancelled' ].includes(j.conclusion))
