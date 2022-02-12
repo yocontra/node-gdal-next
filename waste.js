a7c602aa187100c512a26a74bbded5e1806141f7
Momtchil Momtchev <momtchil@momtchev.com>
2022-02-12 02:07:20 +0100

    remove junk

--- waste.js
+++
@@ -1,9 +0,0 @@
-function waste(print) {
-  const buf = Buffer.alloc(1024 * 1024)
-  if (print) console.log(typeof buf)
-}
-
-for (let i = 0; i < 10e3; i++) {
-  waste(i % 1e3 == 0)
-  global.gc()
-}
