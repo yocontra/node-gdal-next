4a1bd6d4fd7af5487b7202e5e74e8668c8079677
Momtchil Momtchev <momtchil@momtchev.com>
2022-02-23 23:36:00 +0100

    disable wrapVRT testing on older GDAL

--- test/api_wrapVRT.test.ts
+++ test/api_wrapVRT.test.ts
@@ -1,10 +1,14 @@
 import * as gdal from '..'
 import * as chai from 'chai'
+import * as semver from 'semver'
 const assert = chai.assert
 
 describe('gdal.wrapVRT', () => {
 
-  it('should produce identical Dataset by default', () => {
+  it('should produce identical Dataset by default', function () {
+    if (!semver.gte(gdal.version, '3.4.0')) {
+      this.skip()
+    }
     const expected = `<VRTDataset rasterXSize="984" rasterYSize="804">
   <SRS>PROJCS["unnamed",GEOGCS["GRS 1980(IUGG, 1980)",DATUM["unknown",SPHEROID["GRS80",6378137,298.257222101],TOWGS84[0,0,0,0,0,0,0]],PRIMEM["Greenwich",0],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]]],PROJECTION["Albers_Conic_Equal_Area"],PARAMETER["latitude_of_center",23],PARAMETER["longitude_of_center",-96],PARAMETER["standard_parallel_1",29.5],PARAMETER["standard_parallel_2",45.5],PARAMETER["false_easting",0],PARAMETER["false_northing",0],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["Easting",EAST],AXIS["Northing",NORTH]]</SRS>
   <GeoTransform>-1134675.2952829634, 7.502071930146189, 0, 2485710.4658232867, 0, -7.502071930145942</GeoTransform>
@@ -22,7 +26,10 @@ describe('gdal.wrapVRT', () => {
       expected)
   })
 
-  it('should apply a pixel function when specified', () => {
+  it('should apply a pixel function when specified', function () {
+    if (!semver.gte(gdal.version, '3.4.0')) {
+      this.skip()
+    }
     const expected = `<VRTDataset rasterXSize="984" rasterYSize="804">
   <SRS>PROJCS["unnamed",GEOGCS["GRS 1980(IUGG, 1980)",DATUM["unknown",SPHEROID["GRS80",6378137,298.257222101],TOWGS84[0,0,0,0,0,0,0]],PRIMEM["Greenwich",0],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]]],PROJECTION["Albers_Conic_Equal_Area"],PARAMETER["latitude_of_center",23],PARAMETER["longitude_of_center",-96],PARAMETER["standard_parallel_1",29.5],PARAMETER["standard_parallel_2",45.5],PARAMETER["false_easting",0],PARAMETER["false_northing",0],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["Easting",EAST],AXIS["Northing",NORTH]]</SRS>
   <GeoTransform>-1134675.2952829634, 7.502071930146189, 0, 2485710.4658232867, 0, -7.502071930145942</GeoTransform>
@@ -52,7 +59,10 @@ describe('gdal.wrapVRT', () => {
     }), expected)
   })
 
-  it('should support multiband files', () => {
+  it('should support multiband files', function () {
+    if (!semver.gte(gdal.version, '3.4.0')) {
+      this.skip()
+    }
     const expected = `<VRTDataset rasterXSize="512" rasterYSize="512">
   <SRS>EPSG:3857</SRS>
   <GeoTransform>-11584184.5107, 30.00058143924877, 0, 5635549.221409999, 0, -30.00058143924877</GeoTransform>
