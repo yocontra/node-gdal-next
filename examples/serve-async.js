503eb362bd0c87b9082839332bfacd5aa68b7b7e
Momtchil Momtchev <momtchil@momtchev.com>
2023-02-21 11:50:09 +0100

    add coordinate transformation examples
    Resolves #70

--- examples/serve-async.js
+++ examples/serve-async.js
@@ -10,43 +10,53 @@ const path = require('path')
 
 const datasetFile = path.join(__dirname, '..', 'test', 'data', 'sample.tif')
 const port = 8010
+const wgs84 = gdal.SpatialReference.fromEPSG(4326)
 
 gdal.openAsync(datasetFile).then((ds) => {
+  const xform = new gdal.CoordinateTransformation(wgs84, ds)
   console.log(`Opened ${datasetFile}`)
 
   const server = http.createServer(async (req, res) => {
-    const params = req.url.split('/')
-    if (params.length > 2 && params[1].length && params[2].length
-        && !isNaN(params[1]) && ! isNaN(params[2])) {
-      let band, value
+    try {
+      const params = req.url.split('/')
+      if (params.length > 3 && params[1].length && params[2].length
+        && !isNaN(params[2]) && ! isNaN(params[3])) {
 
-      try {
-        band = await ds.bands.getAsync(1)
-      } catch (e) {
-        res.writeHead(500)
-        res.end(e.toString())
-        return
+        let x, y
+        switch (params[1]) {
+          case 'pixels':
+            x = +params[2]
+            y = +params[3]
+            break
+          case 'WGS84':
+            {
+              const coords = xform.transformPoint(+params[2], +params[3])
+              x = Math.round(coords.x)
+              y = Math.round(coords.y)
+            }
+            break
+          default:
+            throw new RangeError('Unsupported method')
+        }
+
+        const band = await ds.bands.getAsync(1)
+        const value = await band.pixels.getAsync(x, y)
+
+        res.writeHead(200)
+        res.end(`Value at position ${x}:${y} is ${value}\n`)
+      } else {
+        throw new TypeError('No or invalid coordinates specified')
       }
-
-      try {
-        value = await band.pixels.getAsync(+params[1], +params[2])
-      } catch (e) {
-        res.writeHead(404)
-        res.end(e.toString())
-        return
-      }
-
-      res.writeHead(200)
-      res.end(`Value at position ${params[1]}:${params[2]} is ${value}\n`)
-
-    } else {
-      res.writeHead(400)
-      res.end('No or invalid coordinates specified')
+    } catch (e) {
+      res.writeHead(e instanceof RangeError ? 404 : 500)
+      res.end(e.toString())
     }
   })
 
   server.listen(port, 'localhost', () => {
     console.log(`Server up and running on http://localhost:${port}`)
-    console.log(`Try  curl http://localhost:${port}/120/100`)
+    console.log('Try:')
+    console.log(`curl http://localhost:${port}/pixels/120/100`)
+    console.log(`curl http://localhost:${port}/WGS84/-110.3/44.55`)
   })
 })
--- examples/serve.js
+++ examples/serve.js
@@ -10,28 +10,53 @@ const path = require('path')
 
 const datasetFile = path.join(__dirname, '..', 'test', 'data', 'sample.tif')
 const port = 8010
+const wgs84 = gdal.SpatialReference.fromEPSG(4326)
 
 gdal.openAsync(datasetFile).then((ds) => {
+  const xform = new gdal.CoordinateTransformation(wgs84, ds)
   console.log(`Opened ${datasetFile}`)
 
   const server = http.createServer((req, res) => {
     const params = req.url.split('/')
-    if (params.length > 2 && params[1].length && params[2].length
-        && !isNaN(params[1]) && ! isNaN(params[2])) {
+    if (params.length > 3 && params[1].length && params[2].length
+        && !isNaN(params[2]) && ! isNaN(params[3])) {
+
+      let x, y
+      switch (params[1]) {
+        case 'pixels':
+          x = +params[2]
+          y = +params[3]
+          break
+        case 'WGS84':
+          try {
+            const coords = xform.transformPoint(+params[2], +params[3])
+            x = Math.round(coords.x)
+            y = Math.round(coords.y)
+          } catch (e) {
+            res.writeHead(500)
+            res.end(e.toString())
+            return
+          }
+          break
+        default:
+          res.writeHead(404)
+          res.end('Unsupported method')
+          return
+      }
 
       ds.bands.getAsync(1)
         .catch((e) => {
           e.httpCode = 500
           throw e
         })
-        .then((band) => band.pixels.getAsync(+params[1], +params[2]))
+        .then((band) => band.pixels.getAsync(x, y))
         .catch((e) => {
           if (!e.httpCode) e.httpCode = 404
           throw e
         })
         .then((value) => {
           res.writeHead(200)
-          res.end(`Value at position ${params[1]}:${params[2]} is ${value}\n`)
+          res.end(`Value at position ${x}:${y} is ${value}\n`)
         })
         .catch((e) => {
           res.writeHead(e.httpCode)
@@ -45,6 +70,8 @@ gdal.openAsync(datasetFile).then((ds) => {
 
   server.listen(port, 'localhost', () => {
     console.log(`Server up and running on http://localhost:${port}`)
-    console.log(`Try  curl http://localhost:${port}/120/100`)
+    console.log('Try:')
+    console.log(`curl http://localhost:${port}/pixels/120/100`)
+    console.log(`curl http://localhost:${port}/WGS84/-110.3/44.55`)
   })
 })
